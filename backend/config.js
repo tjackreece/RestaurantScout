a09b4266e22a59f02e857366530685bdc2d8aefe
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-08-07 09:23:11 -0400

    makingBackend

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	PORT: process.env.PORT || 5000,
+	PGUSER: process.env.PGUSER,
+	PGHOST: process.env.PGHOST,
+	PGDATABASE: process.env.PGDATABASE,
+	PGPASSWORD: process.env.PGPASSWORD,
+	PGPORT: process.env.PGPORT,
+};
